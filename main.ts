60d75c5a25a08281cbfb3c671a8bf9f6303b704c
stackinspector <stackinspector@126.com>
2026-07-22 22:41:23 +0800

    rename do-calc to flip

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ import { flip } from "./wasm/shakenc_flip_web.js";
 
 const input_element = document.getElementById("input")! as HTMLTextAreaElement;
 const output_element = document.getElementById("output")!;
-const do_calc_element = document.getElementById("do-calc")! as HTMLButtonElement;
+const flip_element = document.getElementById("flip")! as HTMLButtonElement;
 const add_time_element = document.getElementById("add-time")! as HTMLButtonElement;
 const clear_element = document.getElementById("clear")! as HTMLButtonElement;
 
-do_calc_element.onclick = () => {
+flip_element.onclick = () => {
     output_element.innerText = flip(input_element.value) ? "正面" : "反面";
 }
 
