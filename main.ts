84ffe279449bda4600aead6f3c9dc5ec208ed7b5
stackinspector <stackinspector@126.com>
2026-07-22 21:48:54 +0800

    multi lines, addtime

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 import { Custom } from "./wasm/cshake_web.js";
 
 declare global {
-    var calc: typeof doCalc;
+    var doCalc: typeof do_calc;
+    var addTime: typeof add_time;
 }
 
-const doCalc = () => {
-    const input_element = document.getElementById("input")! as HTMLInputElement;
-    const output_element = document.getElementById("output")!;
+const input_element = document.getElementById("input")! as HTMLTextAreaElement;
+const output_element = document.getElementById("output")!;
+const text_encoder = new TextEncoder();
+const cshake_custom = Custom.from_string("", "__shakenc__random-generator");
+
+const do_calc = () => {
     const input_text = input_element.value;
-    const text_encoder = new TextEncoder();
     const input_binary = text_encoder.encode(input_text);
-    const cshake_custom = Custom.from_string("", "__shakenc__random-generator");
     const output_bytes = cshake_custom.once_to_bytes(input_binary, 1);
     const output_byte = output_bytes[0];
     const output_bool = (output_byte <= 127) ? true : false;
@@ -18,4 +20,9 @@ const doCalc = () => {
     output_element.innerText = output_text;
 }
 
-globalThis.calc = doCalc;
+const add_time = () => {
+    input_element.value = (new Date()).toISOString() + " " + input_element.value;
+}
+
+globalThis.doCalc = do_calc;
+globalThis.addTime = add_time;
